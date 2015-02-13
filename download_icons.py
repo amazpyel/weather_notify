6e1e2628e7f5ad17eaef60e02ecf334716567b57
Alex <o.pylkevych@gmail.com>
2015-02-13 12:07:07 +0200

    Create download_icons.py

---
+++ download_icons.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+parentdir = os.path.dirname(os.path.realpath(__file__))
+base_icons_url = "http://openweathermap.org/img/w/"
+code = [
+    "01d", "01n",
+    "02d", "02n",
+    "03d", "03n",
+    "04d", "04n",
+    "09d", "09n",
+    "10d", "10n",
+    "11d", "11n",
+    "13d", "13n",
+    "50d", "50n",
+    ]
+
+
+def dowload_icons():
+    try:
+        os.makedirs(parentdir + "/icons/")
+        for i in range(len(code)):
+            f = open(parentdir + "/icons/" + code[i] + ".png", 'wb')
+            f.write(requests.get(base_icons_url + code[i] + ".png").content)
+            f.close()
+    except OSError:
+        print "Icons is already available!"
